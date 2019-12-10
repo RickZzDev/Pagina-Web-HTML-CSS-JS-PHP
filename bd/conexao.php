d9a8a2f84aa678897f7ccc0f744ef7350ca67f23
Carlos Henrique Simões Novais <rick_teloon@hotmail.com>
2019-12-10 19:49:48 -0300

    Add files via upload

--- bd/conexao.php
+++ bd/conexao.php
@@ -2,9 +2,9 @@
 
     
     function conexaoMySql(){
-        $host = (String) "localhost:8080";//onde esta o banco
+        $host = (String) "127.0.0.1";//onde esta o banco
         $user = (String) "root";//usuario para entrar no banco 
-        $password = (String) "";//senha para entrar no banco
+        $password = (String) "bcd127";//senha para entrar no banco
         $database = (String) "dbcontatos20192tb";//nome do banco
         
         $conexao = mysqli_connect($host,$user,$password,$database);
